4770d8e858ca8e4b129bf3f493d966631d07519e
lucandy47 <luciantir98@gmail.com>
2023-06-03 18:15:54 +0300

    Add ui devops files

---
+++ TravelPics.UI/travelpics-ui.bat
@@ -0,0 +1,15 @@
+@ECHO OFF
+set environment=%1
+set imageVersion=%2
+TITLE "travelpics-ui-%environment%"
+ECHO "Image for travelpics-ui-%environment%:%imageVersion% will be created and pushed into the travelpicscntapps container registry"
+ECHO "Creating the image..."
+ECHO OFF
+docker build -t travelpics-ui-%environment%:%imageVersion% -f ./Dockerfile .
+ECHO "Logging into the container registry.. "
+ECHO OFF
+docker login -u travelpicscntapps -p hcKsfCPLqx/kjAZrnEEzqaHpuUAkH3oeEMwJAEHx3T+ACRDMHaIU travelpicscntapps.azurecr.io
+docker tag travelpics-ui-%environment%:%imageVersion% travelpicscntapps.azurecr.io/travelpics-ui-%environment%:%imageVersion%
+ECHO "Pushing image to the repository..."
+ECHO OFF
+docker push travelpicscntapps.azurecr.io/travelpics-ui-%environment%:%imageVersion%
