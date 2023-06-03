a0846d1eaf073760b7d37f5d1016ebcfc4a06b7f
Lucian Ilie Tiron <luciantir98@gmail.com>
2023-06-03 11:18:28 +0300

    update structure

--- API/travelpics-notifications.bat
+++ API/travelpics-notifications.bat
@@ -5,7 +5,7 @@ TITLE "travelpics-notifications-%environment%"
 ECHO "Image for travelpics-notifications-%environment%:%imageVersion% will be created and pushed into the pgpcntapps container registry"
 ECHO "Creating the image..."
 ECHO OFF
-docker build -t travelpics-notifications-%environment%:%imageVersion% -f ./Dockerfile .
+docker build -t travelpics-notifications-%environment%:%imageVersion% -f ./Dockerfile_notifications.dockerfile .
 ECHO "Logging into the container registry.. "
 ECHO OFF
 docker login -u MainToken -p hcKsfCPLqx/kjAZrnEEzqaHpuUAkH3oeEMwJAEHx3T+ACRDMHaIU travelpicscntapps.azurecr.io
