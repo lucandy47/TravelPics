e4737485211bd9263d110068fb9aea581be79322
Lucian Ilie Tiron <luciantir98@gmail.com>
2023-06-03 11:06:21 +0300

    Add notifications devops files

---
+++ API/TravelPics.Notifications/travelpics-notifications.bat
@@ -0,0 +1,15 @@
+@ECHO OFF
+set environment=%1
+set imageVersion=%2
+TITLE "travelpics-notifications-%environment%"
+ECHO "Image for travelpics-notifications-%environment%:%imageVersion% will be created and pushed into the pgpcntapps container registry"
+ECHO "Creating the image..."
+ECHO OFF
+docker build -t travelpics-notifications-%environment%:%imageVersion% -f ./Dockerfile .
+ECHO "Logging into the container registry.. "
+ECHO OFF
+docker login -u MainToken -p hcKsfCPLqx/kjAZrnEEzqaHpuUAkH3oeEMwJAEHx3T+ACRDMHaIU travelpicscntapps.azurecr.io
+docker tag travelpics-notifications-%environment%:%imageVersion% travelpicscntapps.azurecr.io/travelpics-notifications-%environment%:%imageVersion%
+ECHO "Pushing image to the repository..."
+ECHO OFF
+docker push travelpicscntapps.azurecr.io/travelpics-notifications-%environment%:%imageVersion%
