5308d2189380cedbfd161cdd1ad8cfe76784a745
lucandy47 <luciantir98@gmail.com>
2023-06-09 14:08:45 +0300

    add page-not-found

--- TravelPics.UI/src/app/page-not-found/page-not-found.component.ts
+++ TravelPics.UI/src/app/page-not-found/page-not-found.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
   styleUrls: ['./page-not-found.component.scss']
 })
-export class PageNotFoundComponent {
+export class PageNotFoundComponent implements OnInit {
+  public pageUri: string | null = null;
 
+  constructor(private router: Router) {
+    this.pageUri = this.router.getCurrentNavigation()?.extras.state?.pageUri;
+  }
+  ngOnInit(): void {}
 }
